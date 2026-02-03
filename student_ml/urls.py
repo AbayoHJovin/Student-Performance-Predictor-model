71ecdd0fbdbdb334b1e2edce90d1bfb90d730959
AbayoHJovin <abayohirwajovin@gmail.com>
2026-02-03 08:30:34 +0200

    Implemented the UI

--- student_ml/urls.py
+++ student_ml/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('performance.urls')),
+    path('', include('performance.urls')),
 ]
